9cf2cdffbdad00889342cb174e52eab2f353c78b
Arseniy Filatov <factores175@gmail.com>
2024-05-29 13:21:22 +0300

    fix: routing

--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -2,6 +2,6 @@ import { CreatePage } from './page'
 
 // eslint-disable-next-line import/no-anonymous-default-export, import/no-default-export
 export default {
-  path: 'create',
+  path: '/create',
   element: <CreatePage />,
 }
--- src/pages/facility/index.tsx
+++ src/pages/facility/index.tsx
@@ -2,7 +2,7 @@ import { facilityLoader, FacilityPage } from './page'
 
 // eslint-disable-next-line import/no-anonymous-default-export, import/no-default-export
 export default {
-  path: 'facility/:facilityId',
+  path: '/facility/:facilityId',
   loader: facilityLoader,
   element: <FacilityPage />,
 }
